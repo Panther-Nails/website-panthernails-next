2d7da284701dd88697cf6536ba34d6551f7d09e6
PN DevOps Web User <pndevopswebuser@panthernails.com>
2024-04-24 12:42:13 +0530

    App.js and Dyanmic component import added to DynamicPage.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import GlobalStyles from "styles/GlobalStyles";
-import { css } from "styled-components/macro"; //eslint-disable-line
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GlobalStyles from "styles/GlobalStyles";
+import DynamicPage from "DynamicPage";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Test from "pages/Test";
 
 export default function App() {
   return (
@@ -14,29 +12,11 @@ export default function App() {
       <GlobalStyles />
       <Router>
         <Routes>
-          {/* <Route
-            path="/components/:type/:subtype/:name"
-            element={<ComponentRenderer />}
-          />
-          <Route
-            path="/components/:type/:name"
-            element={<ComponentRenderer />}
-          /> */}
-
-          {/* <Route path="/" element={<DynamicPage />}>
-              <Route path="/:type" element={<DynamicPage />} />
-              <Route path="/:type/:subtype" element={<DynamicPage />} />
-              <Route path="/:type/:subtype/:name" element={<DynamicPage />} />
-            </Route> */}
-
-          <Route path="/" element={<Test />}>
-            <Route path="/:type" element={<Test />} />
-            <Route path="/:type/:subtype" element={<Test />} />
-            <Route path="/:type/:subtype/:name" element={<Test />} />
+          <Route path="/" element={<DynamicPage />}>
+            <Route path="/:type" element={<DynamicPage />} />
+            <Route path="/:type/:subtype" element={<DynamicPage />} />
+            <Route path="/:type/:subtype/:name" element={<DynamicPage />} />
           </Route>
-
-          {/* <Route path="MainLandingPage" element={<MainLandingPage />} />
-          <Route path="/:type" element={<ComponentList />} /> */}
         </Routes>
       </Router>
     </>
--- src/DynamicPage.js
+++ src/DynamicPage.js
@@ -1,17 +1,27 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import { Suspense } from "react";
-
 import { ExecuteQuery } from "services/APIService";
-import { getComponentName } from "components/DynamicControls/ComponentsControls";
 import { lazy } from "react";
-import { components } from "ComponentRenderer";
+import GetStarted from "components/cta/GetStarted";
 
 export default () => {
   const { type, subtype, name } = useParams();
 
   const [data, setData] = useState([]);
+
+  const ImportDynamicComponent = (Section, ComponentName) => {
+    const Component = lazy(() =>
+      import(`components/${Section}/${ComponentName}.js`)
+        .then((module) => ({ default: module.default }))
+        .catch((error) => {
+          console.error(`Error loading component ${ComponentName}:`, error);
+          return { default: () => <GetStarted /> }; // to be replaced with ErrorPage
+        })
+    );
+    return Component;
+  };
 
   useEffect(() => {
     ExecuteQuery({
@@ -23,6 +33,13 @@ export default () => {
       console.log("Response", response);
       if (response.message === "Successfull") {
         setData(response.items);
+      } else {
+        setData([
+          {
+            Section: "cta",
+            ComponentName: "GetStarted", // add page not available component
+          },
+        ]);
       }
     });
   }, []);
@@ -40,12 +57,21 @@ export default () => {
 
     return (
       <>
-        {data.map((component, index) => {
-          // console.log("componentsData", component);
-          var Component = getComponentName[component.ComponentName];
-          // console.log("component Name", component.ComponentName);
-          return Component && <Component data={component} />;
-        })}
+        <AnimationRevealPage>
+          {data.map((component) => {
+            console.log("component.Section", component.Section);
+            console.log("component.ComponentName", component.ComponentName);
+            const Component = ImportDynamicComponent(
+              component.Section,
+              component.ComponentName
+            );
+            return (
+              <Suspense>
+                <Component data={component.CPJSON} />
+              </Suspense>
+            );
+          })}
+        </AnimationRevealPage>
       </>
     );
   } catch (e) {
@@ -53,3 +79,31 @@ export default () => {
     return <div>Error: Component Not Found</div>;
   }
 };
+
+/*
+
+Data : 
+1 Home /
+{
+  Title
+  Keyword
+  description
+  components: [
+    {
+      ComponentOrder
+      Section
+      ComponentName
+      ComponentPropertyJSON
+      ComponentHierarchyPropertyJson
+    },
+    {
+      ComponentOrder
+      Section
+      ComponentName
+      ComponentPropertyJSON
+      ComponentHierarchyPropertyJson
+    },
+  ]
+}
+
+*/
