886afd1a1821a75b0b483e1ab13c2691ef207323
Dev Ops Web User <pndevopswebuser@panthernails.com>
2024-05-09 18:21:14 +0530

    gaurav-v2 //all card section component are completed

---
+++ src/components/DynamicCards/ThreeColSliderCard.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import tw from "twin.macro";
+import styled from "styled-components";
+import { SectionHeading } from "components/misc/Headings";
+import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons";
+import { ReactComponent as PriceIcon } from "feather-icons/dist/icons/dollar-sign.svg";
+import { ReactComponent as PinIcon } from "feather-icons/dist/icons/bell.svg";
+import { ReactComponent as LocationIcon } from "feather-icons/dist/icons/map-pin.svg";
+import { ReactComponent as StarIcon } from "feather-icons/dist/icons/star.svg";
+import { ReactComponent as ChevronLeftIcon } from "feather-icons/dist/icons/chevron-left.svg";
+import { ReactComponent as ChevronRightIcon } from "feather-icons/dist/icons/chevron-right.svg";
+import { ReactComponent as PlayIcon } from "feather-icons/dist/icons/play-circle.svg";
+
+
+const Container = tw.div`relative`;
+const Content = tw.div`max-w-screen-xl mx-auto py-16 lg:py-20`;
+
+const HeadingWithControl = tw.div`flex flex-col items-center sm:items-stretch sm:flex-row justify-between`;
+const Heading = tw(SectionHeading)``;
+const Controls = tw.div`flex items-center`;
+const ControlButton = styled(PrimaryButtonBase)`
+  ${tw`mt-4 sm:mt-0 first:ml-0 ml-6 rounded-full p-2`}
+  svg {
+    ${tw`w-6 h-6`}
+  }
+`;
+const PrevButton = tw(ControlButton)``;
+const NextButton = tw(ControlButton)``;
+
+const CardSlider = styled(Slider)`
+  ${tw`mt-16`}
+  .slick-track { 
+    ${tw`flex`}
+  }
+  .slick-slide {
+    ${tw`h-auto flex justify-center mb-1`}
+  }
+`;
+const Card = tw.div`h-full flex! flex-col sm:border max-w-sm sm:rounded-tl-4xl sm:rounded-br-5xl relative focus:outline-none`;
+const CardImage = styled.div(props => [
+  `background-image: url("${props.imageSrc}");`,
+  tw`w-full h-56 sm:h-64 bg-cover bg-center rounded sm:rounded-none sm:rounded-tl-4xl`
+]);
+
+const TextInfo = tw.div`py-6 sm:px-10 sm:py-6`;
+const TitleReviewContainer = tw.div`flex flex-col sm:flex-row sm:justify-between sm:items-center`;
+const Title = tw.h5`text-2xl font-bold`;
+
+const RatingsInfo = styled.div`
+  ${tw`flex items-center sm:ml-4 mt-2 sm:mt-0`}
+  svg {
+    ${tw`w-6 h-6 text-yellow-500 fill-current`}
+  }
+`;
+const Rating = tw.span`ml-2 font-bold`;
+
+const Description = tw.p`text-sm leading-loose mt-2 sm:mt-4`;
+
+const SecondaryInfoContainer = tw.div`flex flex-col sm:flex-row mt-2 sm:mt-4`;
+const IconWithText = tw.div`flex items-center mr-6 my-2 sm:my-0`;
+const IconContainer = styled.div`
+  ${tw`inline-block rounded-full p-2 mx-2 bg-gray-700 text-gray-100`}
+  svg {
+    ${tw`w-3 h-3`}
+  }
+`;
+const Text = tw.div` text-sm font-semibold text-gray-800`;
+
+const PrimaryButton = tw(PrimaryButtonBase)`mt-auto sm:text-lg rounded-none w-full rounded sm:rounded-none sm:rounded-br-4xl py-3 sm:py-6`;
+
+const KnowMoreLink = styled.a`
+  ${tw`mt-4 sm:mt-0  flex items-center text-secondary-300 transition duration-300 hocus:text-primary-400 focus:outline-none`}
+  .playText {
+    ${tw`text-lg `}
+  }
+`;
+export default ({ index, properties, children }) => {
+//   console.log("properties", properties);
+//   console.log("children alternate", children);
+  return (
+    <Card key={index}>
+              <CardImage imageSrc={properties.imageSrc} />
+              <TextInfo>
+                <TitleReviewContainer>
+                  <Title>{properties.title}</Title>
+                  <RatingsInfo>
+                    <StarIcon />
+                    <Rating>{properties.rating}</Rating>
+                  </RatingsInfo>
+                </TitleReviewContainer>
+                <SecondaryInfoContainer>                  
+                   <IconWithText>
+                    <Text>{properties.locationText}</Text>
+                    {properties.knowMoreLinkUrl}
+                  </IconWithText>
+                  <IconWithText>
+                    <IconContainer>
+                      <PriceIcon />
+                    </IconContainer>
+                    <Text>{properties.pricingText}</Text>
+                  </IconWithText>
+                </SecondaryInfoContainer>
+                <Description>                  
+                  {properties.description}
+                <KnowMoreLink href={properties.linkUrl} >
+                  <span className="playText">{properties.linkText}</span>
+                </KnowMoreLink>
+                </Description>
+                
+              </TextInfo>              
+              <PrimaryButton>Book a Meeting</PrimaryButton>
+            </Card>
+  );
+};
