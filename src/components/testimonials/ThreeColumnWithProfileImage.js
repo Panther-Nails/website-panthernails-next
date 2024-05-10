8c373dfc3745344be06c5c1f09ac75325f50236d
Dev Ops Web User <pndevopswebuser@panthernails.com>
2024-05-10 17:53:49 +0530

    gaurav-v2 opacity cheneg

--- src/components/testimonials/ThreeColumnWithProfileImage.js
+++ src/components/testimonials/ThreeColumnWithProfileImage.js
@@ -22,10 +22,10 @@ const Quote = tw.blockquote`mt-5 text-gray-600 font-medium leading-loose`;
 const CustomerName = tw.p`mt-5 text-gray-900 font-semibold uppercase text-sm tracking-wide`;
 
 const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
-  ${tw`pointer-events-none -z-20 absolute left-0 top-0 h-56 w-56 opacity-15 transform -translate-x-2/3 -translate-y-12 text-teal-400`}
+  ${tw`pointer-events-none -z-20 absolute left-0 top-0 h-56 w-56 opacity-10 transform -translate-x-2/3 -translate-y-12 text-teal-400`}
 `;
 const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
-  ${tw`pointer-events-none -z-20 absolute right-0 bottom-0 h-64 w-64 opacity-15 transform translate-x-2/3 text-yellow-500`}
+  ${tw`pointer-events-none -z-20 absolute right-0 bottom-0 h-64 w-64 opacity-10 transform translate-x-2/3 text-yellow-500`}
 `;
 
 export default ({ properties, children }) => {
