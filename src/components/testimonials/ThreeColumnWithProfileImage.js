60653d484646bba80d28cfd93fb7f36b96272153
Dev Ops Web User <pndevopswebuser@panthernails.com>
2024-05-10 17:21:47 +0530

    gaurav-v2 //section heading color chenge and ThreeColumnWithProfileImage add heading style

--- src/components/testimonials/ThreeColumnWithProfileImage.js
+++ src/components/testimonials/ThreeColumnWithProfileImage.js
@@ -4,13 +4,15 @@ import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
 import { ContentWithPaddingXl, Container } from "components/misc/Layouts.js";
 import {
-  SectionHeading as Heading,
+  SectionHeading ,
   Subheading as SubheadingBase,
 } from "components/misc/Headings.js";
 import { ReactComponent as SvgDecoratorBlob1 } from "images/svg-decorator-blob-7.svg";
 import { ReactComponent as SvgDecoratorBlob2 } from "images/svg-decorator-blob-8.svg";
 import { getProperties } from "services/JsonService";
-
+const Heading = tw(
+  SectionHeading
+)`mt-4 font-black text-3xl sm:text-4xl lg:text-5xl text-center  leading-tight`;
 const Subheading = tw(SubheadingBase)`text-center`;
 const Testimonials = tw.div`flex flex-col lg:flex-row items-center lg:items-stretch`;
 const TestimonialContainer = tw.div`mt-16 lg:w-1/3`;
