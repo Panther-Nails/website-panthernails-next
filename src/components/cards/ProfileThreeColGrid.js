f914bea74834cd608b8d8cd75dde8bd4237b9e29
PN DevOps Web User <pndevopswebuser@panthernails.com>
2024-06-06 13:34:49 +0530

    change heading from profileThreeColGrid to HightlightHeading

--- src/components/cards/ProfileThreeColGrid.js
+++ src/components/cards/ProfileThreeColGrid.js
@@ -3,7 +3,7 @@ import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
 import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
 import {
-  SectionHeading,
+  HighlightedHeading,
   Subheading as SubheadingBase,
 } from "components/misc/Headings";
 import { SectionDescription } from "components/misc/Typography";
@@ -16,32 +16,6 @@ const Description = tw(SectionDescription)`mx-auto text-center`;
 
 const Cards = tw.div`flex flex-wrap flex-row justify-center sm:max-w-2xl lg:max-w-5xl mx-auto`;
 
-const Heading = tw(
-  SectionHeading
-)`w-full mt-4 font-black px-4 lg:px-0 text-2xl sm:text-4xl lg:text-5xl text-center leading-tight `;
-
-const HighlightedHeading = ({ children }) => {
-  return (
-    <>
-      {children.split("`").map((chunk, index) => {
-        console.log(chunk);
-        if (index % 2 === 0)
-          return (
-            <span key={index} tw="text-primary-900">
-              {chunk}
-            </span>
-          );
-        else
-          return (
-            <span key={index} tw="text-primary-500">
-              {chunk}
-            </span>
-          );
-      })}
-    </>
-  );
-};
-
 export default ({ index, children, properties }) => {
   return (
     <Container>
@@ -51,9 +25,7 @@ export default ({ index, children, properties }) => {
             <Subheading>{properties.subHeading}</Subheading>
           )}
           {properties.heading && (
-            <Heading>
-              <HighlightedHeading>{properties.heading}</HighlightedHeading>
-            </Heading>
+            <HighlightedHeading>{properties.heading}</HighlightedHeading>
           )}
           {properties.description && (
             <Description>{properties.description}</Description>
