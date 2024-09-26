56bfb8d9ddc8a104cc07a6a51823d9d865d9d551
Dev Ops Web User <pndevopswebuser@panthernails.com>
2024-09-26 14:48:21 +0530

    gaurav-v2 //after merge give error releted heading in profile threeColGrid solve it

--- src/components/cards/ProfileThreeColGrid.js
+++ src/components/cards/ProfileThreeColGrid.js
@@ -2,6 +2,7 @@ import React from "react";
 import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
 import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
+import { SectionHeading as HeadingBase } from "components/misc/Headings";
 import {
   HighlightedHeading,
   Subheading as SubheadingBase,
@@ -14,31 +15,6 @@ const Subheading = tw(SubheadingBase)`text-center mb-3`;
 const Description = tw(SectionDescription)`mx-auto text-center`;
 
 const Cards = tw.div`flex flex-wrap flex-row justify-center sm:max-w-2xl lg:max-w-5xl mx-auto`;
-
-const Heading = tw(
-  SectionHeading
-)`w-full mt-4 font-black px-4 lg:px-0 text-2xl sm:text-4xl lg:text-5xl text-center leading-tight `;
-
-const HighlightedHeading = ({ children }) => {
-  return (
-    <>
-      {children.split("`").map((chunk, index) => {
-        if (index % 2 === 0)
-          return (
-            <span key={index} tw="text-primary-900">
-              {chunk}
-            </span>
-          );
-        else
-          return (
-            <span key={index} tw="text-primary-500">
-              {chunk}
-            </span>
-          );
-      })}
-    </>
-  );
-};
 
 export default ({ index, children, properties }) => {
   return (
